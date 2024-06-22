7bac95209569ef96a28a4d15a7647005d863c895
OwenMac <owen4123@gmail.com>
2024-06-22 08:45:22 +0800

    Added Header, balance and right side bar in Home page; Init other 3 pages route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Owen" };
+  const loggedIn = {
+    firstName: "Owen",
+    lastName: "JSM",
+    email: "contact@mail.com",
+  };
 
   return (
     <section className="home">
@@ -16,13 +21,18 @@ const Home = () => {
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRNX
       </div>
+
+      {/* RightSidebar only use in Home page, so putting RightSidebar here 
+      rather than ./layout.tsx  */}
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
   );
 };
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Trnx = () => {
+  return (
+    <div>Trnx</div>
+  )
+}
+
+export default Trnx
